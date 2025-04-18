d99ecef38dbc50b7aa96b58f9f0b905748eef423
kadu1397 <kaueduardo1397@gmail.com>
2025-04-17 21:20:08 -0300

    aula-08-Rodou

---
+++ Util/bd.py
@@ -0,0 +1,15 @@
+import os
+import yaml
+
+config_path = os.path.join(os.path.dirname(__file__), '..', 'config.yaml')
+
+with open(config_path, 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+# Database configuration
+database_config = config['database']
+host = database_config['host']
+port = database_config['port']
+user = database_config['user']
+password = database_config['password']
+name = database_config['name']
